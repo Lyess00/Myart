9049e7a8157352ce4861179562bd2d8a45845201
Sander-F <sanderf.pro@gmail.com>
2023-03-29 15:11:14 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include "template/header.php";?>
+
+<div>
+    <h1 class="title">MY ART</h1>
+    <h2 class="slogan">Créez, partager, progressez</h2>
+    <button type="button" class="btn btn-grad">EN SAVOIR PLUS</button>
+</div>
