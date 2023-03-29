a9944d3df885744797ad5b51ec2fa1debc35da81
Sander-F <sanderf.pro@gmail.com>
2023-03-29 15:20:48 +0200

    add all

---
+++ Template/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="My Art">
+        <title>MyArt</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat+Subrayada&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="hero">
+            <nav>
+                <h2 class="logo"><img class="myart" src="medias/MyArt.svg"></h2>
+                 
+                <ul>
+                    <li><a href="#">Sign-in</a>
+                    <li><a href="#">About</a>
+                    <li><a href="#">Contact</a>
+                    <li><a class="faq" href="#">FAQ</a>
+                </ul>
+            </nav>
+        </div>
